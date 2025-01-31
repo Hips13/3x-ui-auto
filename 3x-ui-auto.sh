71aa8e247eed49ae06934b72b1a2805e4ce17aa1
Hips13 <vseivse@gmail.com>
2025-01-31 14:09:43 +0300

    Create 3x-ui-auto.sh

---
+++ 3x-ui-auto.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+color_text() {
+    local color=$1
+    local text=$2
+    case $color in
+        green) echo -e "\e[32m$text\e[0m" ;;
+        cyan) echo -e "\e[36m$text\e[0m" ;;
+        *) echo "$text" ;;
+    esac
+}
+
+confirm_continue() {
+    while true; do
+        read -p "Продолжить? (y/n): " yn
+        case $yn in
+            [Yy]* ) break ;;
+            [Nn]* ) echo "Скрипт остановлен."; exit ;;
+            * ) echo "Пожалуйста, введите y или n." ;;
+        esac
+    done
+}
+
+apt update && apt upgrade -y
+
+RANDOM_PORT=$((RANDOM % 30001 + 30000))
+echo "ssh port $RANDOM_PORT"
+
+confirm_continue
+
+SSHD_CONFIG="/etc/ssh/sshd_config"
+sed -i "s/^#Port.*/Port $RANDOM_PORT/" $SSHD_CONFIG
+sed -i "s/^Port.*/Port $RANDOM_PORT/" $SSHD_CONFIG
+
+color_text green "В терминале вашего Windows (правой кнопкой по пуску - терминал) выполнить скрипт для генерации SSH ключа:"
+color_text cyan 'Invoke-WebRequest -Uri "https://raw.githubusercontent.com/hips13/win-ssh/main/create_vpn_ssh_key.ps1" -OutFile "create_vpn_ssh_key.ps1"'
+
+confirm_continue
+
+echo "Введите ваш SSH ключ (завершите ввод пустой строкой):"
+SSH_KEY=""
+while IFS= read -r line; do
+    if [ -z "$line" ]; then
+        break
+    fi
+    SSH_KEY+="$line"$'\n'
+done
+
+mkdir -p /root/.ssh
+echo "$SSH_KEY" >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+
+confirm_continue
+
+sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' $SSHD_CONFIG
+sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' $SSHD_CONFIG
+sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' $SSHD_CONFIG
+sed -i 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' $SSHD_CONFIG
+
+systemctl restart sshd
+
+mkdir -p /etc/ssl/private /etc/ssl/certs
+openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout /etc/ssl/private/private.key -out /etc/ssl/certs/public.key -days 3650 -subj "/CN=APP"
+
+echo "Путь SSL ключа для панели:"
+echo "/etc/ssl/private/private.key"
+echo "/etc/ssl/certs/public.key"
+
+confirm_continue
+
+EXTERNAL_IP=$(curl -s ifconfig.me)
+
+echo "Теперь подключение к серверу будет выполняться командой:"
+echo "ssh root@$EXTERNAL_IP -p $RANDOM_PORT -i \"C:\Users\\%username%\\.ssh\\vpn\""
+
+confirm_continue
+
+bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
