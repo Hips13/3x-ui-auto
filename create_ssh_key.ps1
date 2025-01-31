06180195fd1267639a1c01a02aaa267251d8af17
Hips13 <vseivse@gmail.com>
2025-01-31 13:15:12 +0300

    create_ssh_key.ps1

--- create_ssh_key.ps1
+++ create_ssh_key.ps1
@@ -1,14 +1,18 @@
+# Переход в папку .ssh
 $sshDir = "$env:USERPROFILE\.ssh"
 if (-Not (Test-Path $sshDir)) {
     New-Item -ItemType Directory -Path $sshDir
 }
 cd $sshDir
 
+# Генерация SSH-ключа с именем "vpn" и без пароля
 ssh-keygen -t rsa -b 4096 -C "vpn_key" -f "$sshDir\vpn" -N '""'
 
+# Проверка, создан ли ключ
 if ((Test-Path "$sshDir\vpn") -and (Test-Path "$sshDir\vpn.pub")) {
     Write-Host "SSH-ключ 'vpn' успешно создан в папке $sshDir" -ForegroundColor Green
 
+    # Чтение и вывод публичного ключа зеленым цветом
     $publicKey = Get-Content -Path "$sshDir\vpn.pub"
     Write-Host "Публичная часть ключа:" -ForegroundColor Green
     Write-Host $publicKey -ForegroundColor Green
