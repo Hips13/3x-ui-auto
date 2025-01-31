f32e02c9640cf216b43114a90ba66a498fd3a48d
Hips13 <vseivse@gmail.com>
2025-01-31 12:43:03 +0300

    Create create_ssh_key.ps1

---
+++ create_ssh_key.ps1
@@ -0,0 +1,17 @@
+$sshDir = "$env:USERPROFILE\.ssh"
+if (-Not (Test-Path $sshDir)) {
+    New-Item -ItemType Directory -Path $sshDir
+}
+cd $sshDir
+
+ssh-keygen -t rsa -b 4096 -C "vpn_key" -f "$sshDir\vpn" -N '""'
+
+if (Test-Path "$sshDir\vpn" -and Test-Path "$sshDir\vpn.pub") {
+    Write-Host "SSH-ключ 'vpn' успешно создан в папке $sshDir" -ForegroundColor Green
+
+    $publicKey = Get-Content -Path "$sshDir\vpn.pub"
+    Write-Host "Публичная часть ключа:" -ForegroundColor Green
+    Write-Host $publicKey -ForegroundColor Green
+} else {
+    Write-Host "Ошибка при создании SSH-ключа." -ForegroundColor Red
+}
