f816b0213af23c60743cc8f2c0827f487513c2d9
Hips13 <vseivse@gmail.com>
2025-01-31 13:26:24 +0300

    Update create_ssh_key1.ps1

--- create_ssh_key1.ps1
+++ create_ssh_key1.ps1
@@ -1,20 +1,20 @@
-# Переход в папку .ssh
+# Navigate to the .ssh directory
 $sshDir = "$env:USERPROFILE\.ssh"
 if (-Not (Test-Path $sshDir)) {
     New-Item -ItemType Directory -Path $sshDir
 }
 cd $sshDir
 
-# Генерация SSH-ключа с именем "vpn" и без пароля
+# Generate an SSH key named "vpn" without a passphrase
 ssh-keygen -t rsa -b 4096 -C "vpn_key" -f "$sshDir\vpn" -N '""'
 
-# Проверка, создан ли ключ
+# Check if the key was created successfully
 if ((Test-Path "$sshDir\vpn") -and (Test-Path "$sshDir\vpn.pub")) {
-    Write-Host "SSH-ключ 'vpn' успешно создан в папке $sshDir" -ForegroundColor Green
+    Write-Host "SSH key 'vpn' successfully created in the folder: $sshDir" -ForegroundColor Green
 
-    # Открываем публичный ключ в блокноте
+    # Open the public key in Notepad
     $publicKeyPath = "$sshDir\vpn.pub"
     notepad $publicKeyPath
 } else {
-    Write-Host "Ошибка при создании SSH-ключа." -ForegroundColor Red
+    Write-Host "Error: Failed to create the SSH key." -ForegroundColor Red
 }
